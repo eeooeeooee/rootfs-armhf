1a9fb1a575751f53b7692ad4fd05310aa392a3f1
slitaz-cn <teasiu@163.com>
2021-12-08 00:56:16 +0800

    update

--- 3-blog.sh
+++ 3-blog.sh
@@ -7,7 +7,7 @@ chmod 777 -R blog
 rm 1.1-17.10.30-release.tar.gz
 mv index.nginx-debian.html index.html
 mkdir files
-cd files
-ln -s /mnt/sda1/files/hi3798mv100 hi3798mv100
-ln -s /mnt/sda1/share share
+#cd files
+#ln -s /mnt/sda1/files/hi3798mv100 hi3798mv100
+#ln -s /mnt/sda1/share share
 
